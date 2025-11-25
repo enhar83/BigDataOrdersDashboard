b16a01de975e6c66b62125e92d1d321dbeb2cbdb
user <user@DESKTOP-01TRV82>
2025-11-26 02:25:22 +0300

    Add project files.

---
+++ Presentation Layer/Controllers/StatisticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_Layer.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
