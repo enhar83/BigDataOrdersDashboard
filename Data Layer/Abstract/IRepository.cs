7c19d76becfb302a71d20e6f7f39589a4b3d5885
Enhar Yorulmaz <enharyrlmz@gmail.com>
2025-11-27 23:01:57 +0300

    Repository için gerekli olan Interface (IRepository) oluşturuldu.

---
+++ Data Layer/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer.Abstract
+{
+    public interface IRepository<T> where T: class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAllIncluding(params Expression<Func<T, Object>>[] includeProperties);
+        T GetById (int id);
+        T GetFirstOrDefault(Expression<Func<T, bool>> predicate);
+        void Add (T entity);
+        void Update (T entity);
+        void Delete (T entity);
+
+
+    }
+}
