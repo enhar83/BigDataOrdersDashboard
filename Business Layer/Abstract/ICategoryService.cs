e33015b95ba5fb5664d511ad9bae80eb37e6ab90
Enhar Yorulmaz <enharyrlmz@gmail.com>
2025-11-29 00:51:44 +0300

    CategoryManager ve ICategoryService eklendi.

---
+++ Business Layer/Abstract/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity_Layer;
+
+namespace Business_Layer.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+        Category GetById(int id);
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(int id);
+    }
+}
+
+//Controller'ın doğrudan managera bağımlı olmasını engeller.
+//DI düzgün çalışmasını ve DIP'ı sağlar.
---
+++ Business Layer/Concrete/CategoryManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business_Layer.Abstract;
+using Data_Layer.Abstract;
+using Entity_Layer;
+
+namespace Business_Layer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly IUnitOfWork _uow;
+
+        public CategoryManager(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public void Add(Category category)
+        {
+            if(string.IsNullOrWhiteSpace(category.CategoryName))
+            {
+                throw new Exception("Kategori adı boş olamaz.");
+            }
+
+            _uow.Categories.Add(category);
+            _uow.Save();
+        }
+
+        public void Delete(int id)
+        {
+            _uow.Categories.Delete(id);
+            _uow.Save();
+        }
+
+        public List<Category> GetAll()
+        {
+            return _uow.Categories.GetAll().ToList();
+        }
+
+        public Category GetById(int id)
+        {
+            return _uow.Categories.GetById(id);
+        }
+
+        public void Update(Category category)
+        {
+            if (category.CategoryId <= 0)
+            {
+                throw new Exception("Geçersiz kategori ID'si.");
+            }
+
+            _uow.Categories.Update(category);
+            _uow.Save();
+        }
+    }
+}
