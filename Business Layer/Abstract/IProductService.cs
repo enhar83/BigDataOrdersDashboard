167e530f6418071ed1e4635aa0a8038c418b6eb2
Enhar Yorulmaz <enharyrlmz@gmail.com>
2025-12-01 23:54:16 +0300

    IProductService ve ProductManager tamamlandı, Program.cs kayıtları yapıldı.

---
+++ Business Layer/Abstract/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity_Layer;
+
+namespace Business_Layer.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        List<Product> GetAllWithCategories();
+        Product GetById(int id);
+        Product GetFirstOrDefault(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int id);
+    }
+}
---
+++ Business Layer/Concrete/ProductManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business_Layer.Abstract;
+using Data_Layer.Abstract;
+using Entity_Layer;
+
+namespace Business_Layer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IUnitOfWork _uow;
+
+        public ProductManager(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public void Add(Product product)
+        {
+            _uow.Products.Add(product);
+            _uow.Save();
+        }
+
+        public void Delete(int id)
+        {
+            _uow.Products.Delete(id);
+            _uow.Save();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _uow.Products.GetAll().ToList();
+        }
+
+        public List<Product> GetAllWithCategories()
+        {
+            return _uow.Products.GetAllIncluding(p => p.Category).ToList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _uow.Products.GetById(id);
+        }
+
+        public Product GetFirstOrDefault(int id)
+        {
+            return _uow.Products.GetFirstOrDefault(p => p.ProductId == id);
+        }
+
+        public void Update(Product product)
+        {
+            if (product.ProductId <= 0)
+            {
+                throw new Exception("Geçersiz kategori ID'si.");
+            }
+
+            _uow.Products.Update(product);
+            _uow.Save();
+        }
+    }
+}
