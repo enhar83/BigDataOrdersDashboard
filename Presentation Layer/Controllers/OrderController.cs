154cad6f324d3824bf612e640441caab927877a1
Enhar Yorulmaz <enharyrlmz@gmail.com>
2025-12-05 16:52:19 +0300

    AddOrder, UpdateOrder ve DeleteOrder işlemleri tamamlandı.

--- Presentation Layer/Controllers/OrderController.cs
+++ Presentation Layer/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using Business_Layer.Abstract;
+using Entity_Layer;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation_Layer.Controllers
@@ -23,5 +24,47 @@ namespace Presentation_Layer.Controllers
 
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult AddOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddOrder(Order order)
+        {
+            order.OrderDate = DateTime.Now;
+
+            _orderService.Add(order);
+            return RedirectToAction("OrderList");
+
+        }
+
+        [HttpGet]
+        public IActionResult UpdateOrder(int id)
+        {
+            var order = _orderService.GetById(id);
+            return View(order);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateOrder(Order order)
+        {
+            order.OrderDate = DateTime.Now;
+
+            _orderService.Update(order);
+            return RedirectToAction("OrderList");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteOrder(int id)
+        {
+            _orderService.Delete(id);
+            return RedirectToAction("OrderList");
+        }
     }
 }
+
