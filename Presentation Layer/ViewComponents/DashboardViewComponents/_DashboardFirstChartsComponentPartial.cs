6b98a709cea34e4088bbd43fd298b793843d9aec
Enhar Yorulmaz <enharyrlmz@gmail.com>
2025-12-16 17:10:00 +0300

    Dashboard sayfasında, üstte bulunan 4 chart için component açıldı ve statik şekilde içleri dolduruldu.

---
+++ Presentation Layer/ViewComponents/DashboardViewComponents/_DashboardFirstChartsComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Business_Layer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_Layer.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardFirstChartsComponentPartial:ViewComponent
+    {
+        private readonly IOrderService _orderService;
+
+        public _DashboardFirstChartsComponentPartial(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
