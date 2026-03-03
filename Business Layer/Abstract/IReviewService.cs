bd0fe044d38065680b8479da3debaf52ebac6eb1
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-03 09:49:26 +0300

    IReviewService ve ReviewManager içerisinde CRUD işlemleri tanımlandı.

--- Business Layer/Abstract/IReviewService.cs
+++ Business Layer/Abstract/IReviewService.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity_Layer;
 
 namespace Business_Layer.Abstract
 {
     public interface IReviewService
     {
+        List<Review> GetAll();
+        Review GetById(int id);
+        Review GetFirstOrDefault(int id);
+        void Add(Review review);
+        void Update(Review review);
+        void Delete(int id);
+        (List<Review> reviews, int totalCount) GetReviewsWithPaging(int pageNumber, int pageSize);
     }
 }
--- Business Layer/Concrete/ReviewManager.cs
+++ Business Layer/Concrete/ReviewManager.cs
@@ -5,16 +5,55 @@ using System.Text;
 using System.Threading.Tasks;
 using Business_Layer.Abstract;
 using Data_Layer.Abstract;
+using Entity_Layer;
 
 namespace Business_Layer.Concrete
 {
-    public class ReviewManager:IReviewService
+    public class ReviewManager : IReviewService
     {
         private readonly IUnitOfWork _uow;
 
         public ReviewManager(IUnitOfWork uow)
         {
             _uow = uow;
+        }
+
+        public void Add(Review review)
+        {
+            _uow.Reviews.Add(review);
+            _uow.Save();
+        }
+
+        public void Delete(int id)
+        {
+            _uow.Reviews.Delete(id);
+            _uow.Save();
+        }
+
+        public List<Review> GetAll()
+        {
+            return _uow.Reviews.GetAll().ToList();
+        }
+
+        public Review GetById(int id)
+        {
+            return _uow.Reviews.GetById(id);
+        }
+
+        public Review GetFirstOrDefault(int id)
+        {
+            return _uow.Reviews.GetFirstOrDefault(r => r.ReviewId == id);
+        }
+
+        public (List<Review> reviews, int totalCount) GetReviewsWithPaging(int pageNumber, int pageSize)
+        {
+            return _uow.Reviews.GetAllWithPaging(pageNumber, pageSize);
+        }
+
+        public void Update(Review review)
+        {
+            _uow.Reviews.Update(review);
+            _uow.Save();
         }
     }
 }
