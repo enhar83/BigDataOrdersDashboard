0f66b9045d1c1a6b6f2ae2f68f313d9af836afe3
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-03 22:00:30 +0300

    Dashboard içerisinde son 5 review'ı gösterecek olan viewcomponent ve view'i oluşturuldu, henüz dinamik hale getirilmedi.

---
+++ Presentation Layer/ViewComponents/DashboardViewComponents/_DashboardLast5ReviewsComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Business_Layer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_Layer.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardLast5ReviewsComponentPartial : ViewComponent
+    {
+        private readonly IReviewService _reviewService;
+
+        public _DashboardLast5ReviewsComponentPartial(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
