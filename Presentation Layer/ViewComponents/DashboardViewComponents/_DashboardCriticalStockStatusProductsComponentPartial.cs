d6aa50c12ca1bbb29eb849d5af7d21f713c75052
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-03 22:59:49 +0300

    Dashboard içinde kritik stok durumu olan ürünler için gerekli olan ViewComponent ve View'i oluşturuldu, henüz dinamik hale getirilmedi.

---
+++ Presentation Layer/ViewComponents/DashboardViewComponents/_DashboardCriticalStockStatusProductsComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Business_Layer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_Layer.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardCriticalStockStatusProductsComponentPartial:ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public _DashboardCriticalStockStatusProductsComponentPartial(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IViewComponentResult Invoke()
+        { 
+            return View();
+        }
+    }
+}
