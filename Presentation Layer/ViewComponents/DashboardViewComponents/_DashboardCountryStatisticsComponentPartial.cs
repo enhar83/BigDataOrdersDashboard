8b4bfd530fda288e5538436c6bb047c02f1b6e7b
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-04 09:38:32 +0300

    Dashboard içerisinde ülkelere göre sipariş dağılımlarını gösteren donut için gerekli olan ViewComponent ve view oluşturuldu, henüz dinamik hale getirilmedi.

---
+++ Presentation Layer/ViewComponents/DashboardViewComponents/_DashboardCountryStatisticsComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Business_Layer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_Layer.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardCountryStatisticsComponentPartial:ViewComponent
+    {
+        private readonly IOrderService _orderService;
+
+        public _DashboardCountryStatisticsComponentPartial(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
