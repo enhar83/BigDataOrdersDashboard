63860a6861e6de38b101ee333ffa4f5266000a36
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-04 12:10:50 +0300

    2024 ve 2025 verilerini kullanarak 2026 ilk üç ayındaki ödeme yöntemi tahmini (Time Series) için kullanılacak olan PaymentForecastDataDTO ve PaymentForecastPredictionDTO oluşturuldu.

---
+++ Core Layer/DTOs/DTOsForMachineLearning/PaymentForecastData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.DTOs.DTOsForMachineLearning
+{
+    public class PaymentForecastData
+    {
+        public string PaymentMethod { get; set; }
+        public float MonthIndex { get; set; }
+        public float OrderCount { get; set; }
+    }
+}
+
+/*
+Giriş verisi olarak kullanılacaktır.
+Bu sınıf ML.NET modeline geçmişte ne olduğunu anlatmak için kullanılır.
+    - PaymentMethod: Modelin farklı ödeme yöntemleri arasındaki davranış farklarını anlamasını sağlar.
+    - MonthIndex: Zaman serisi analizlerinde zamanın akışını temsil eder. ML.NET 1. aydan 24. aya kadar olan dizilimi takip ederek artış veya azalış trendini bu index üzerinden yakalar.
+    - OrderCount: Label denilen hedef değeridir. Model, PaymentMethod ve MonthIndex verildiğinde bu rakama nasıl ulaşıldığını öğrenmeye çalışır.
+*/
---
+++ Core Layer/DTOs/DTOsForMachineLearning/PaymentForecastPrediction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.DTOs.DTOsForMachineLearning
+{
+    public class PaymentForecastPrediction
+    {
+        public float[] ForecastedValues { get; set; }
+    }
+}
+
+/*
+Çıkış verisidir. Modelin eğitim bittikten sonra verdiği gelecek raporudur.
+   - ForecastedValues (float[]): Forecasting senaryolarında model genellikle tek bir değer yerine bir array döner. 2026'nın ilk 3 ayı istendiği için, bu dizinin içinde sırasıyla [Ocak_Tahmini, Subat_Tahmini, Mart_Tahmini] değerleri yer alacaktır.
+  
+   - Neden float[]?: ML.NET'in zaman serisi algoritmaları belirli bir zaman penceresi için toplu tahmin üretme eğilimindedir.
+        * Horizon Mantığı: ML.NET zaman serisi algoritmaları, bana bir sonraki adımı söyle demek yerine bana önümüzdeki X birimlik zaman dilimini komple tahmin et mantığıyla çalışır. Ocak, Şubat, Mart tahmin edildiğinden dolayı horizon burada 3tür.
+        * Tahminlerin Birbirine Bağlılığı: Bu modelde Ocak, Şubatı ; Şubat da Martı etkiler. Yani Ocak hesaplandıktan sonra, Şubat hesaplanırken Ocak verisi de kullanılır.
+        * ForecastedValues[0] --> 2026 Ocak Tahmini
+        * ForecastedValues[1] --> 2026 Şubat Tahmini
+        * ForecastedValues[2] --> 2026 Mart Tahmini
+        
+        * ML.NET içerisinde zaman serisi tahminleme için kullanılan TimeSeriesPredictionEngine yapısı, çıktı sınıfında (PaymentForecastPrediction) tahmin edilen değerlerin bir dizi olmasını zorunlu kılar. Eğer bunu tek bir float yaparsan, algoritma 3 aylık tahmini nereye yazacağını bilemez ve hata verir. 
+*/
