2dc51142228eccebe87b53e70bc6a86f91081c53
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-05 15:30:28 +0300

    CustomerAnalytics için controller ve view oluşturuldu. Block Header ve Main Statistics kısmı componentlere ayrıldı, henüz dinamik hale getirilmedi.

---
+++ Presentation Layer/Controllers/CustomerAnalytics.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_Layer.Controllers
+{
+    public class CustomerAnalytics : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
