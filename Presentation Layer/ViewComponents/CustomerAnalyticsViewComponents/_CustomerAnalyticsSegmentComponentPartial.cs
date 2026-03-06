b3eb9abbc23113b3c7de505b3078a520290786ca
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-06 15:55:48 +0300

    CustomerAnalyticsSegment ViewComponent'i oluşturuldu. Statik olarak veriler geliyor. Kendi içerisinde de bölünmeye devam edecek.

---
+++ Presentation Layer/ViewComponents/CustomerAnalyticsViewComponents/_CustomerAnalyticsSegmentComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Business_Layer.MachineLearning.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_Layer.ViewComponents.CustomerAnalyticsViewComponents
+{
+    public class _CustomerAnalyticsSegmentComponentPartial:ViewComponent
+    {
+        private readonly ICustomerAnalyticsService _customerAnalyticsService;
+
+        public _CustomerAnalyticsSegmentComponentPartial(ICustomerAnalyticsService customerAnalyticsService)
+        {
+            _customerAnalyticsService = customerAnalyticsService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
