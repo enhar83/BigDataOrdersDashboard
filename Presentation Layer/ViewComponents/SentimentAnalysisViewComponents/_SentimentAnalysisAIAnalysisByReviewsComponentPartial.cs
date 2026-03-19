9ee6bc0079c985bbd8680a03ed967f791efe8b0f
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-19 11:03:27 +0300

    ReviewInsights için gerekli olan ViewComponent tamamlandı view'i oluşturuldu.

---
+++ Presentation Layer/ViewComponents/SentimentAnalysisViewComponents/_SentimentAnalysisAIAnalysisByReviewsComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Business_Layer.MachineLearning.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation_Layer.ViewComponents.SentimentAnalysisViewComponents
+{
+    public class _SentimentAnalysisAIAnalysisByReviewsComponentPartial:ViewComponent
+    {
+        private readonly ISentimentAnalysisService _sentimentAnalysisService;
+
+        public _SentimentAnalysisAIAnalysisByReviewsComponentPartial(ISentimentAnalysisService sentimentAnalysisService)
+        {
+            _sentimentAnalysisService = sentimentAnalysisService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
