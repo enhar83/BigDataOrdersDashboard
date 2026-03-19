bc67905663842af92987fee385fd50f188a43643
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-19 10:51:00 +0300

    Sipariş bazlı AI analizi tamamlandı, yorum bazlı AI analizi için gerekli olan controller metodu ve view oluşturuldu.

--- Presentation Layer/Controllers/SentimentAnalysisController.cs
+++ Presentation Layer/Controllers/SentimentAnalysisController.cs
@@ -1,5 +1,6 @@
 ﻿using Business_Layer.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace Presentation_Layer.Controllers
 {
@@ -8,9 +9,16 @@ namespace Presentation_Layer.Controllers
         public IActionResult CustomerInsights(int id)
         {
             if (id <= 0)
-            {
-                return RedirectToAction("Index");
-            }
+                return RedirectToAction("Customer/CustomerList");
+
+            ViewBag.SelectedId = id;
+            return View();
+        }
+
+        public IActionResult ReviewInsights(int id)
+        {
+            if (id <= 0) 
+                return RedirectToAction("Customer/CustomerList");
 
             ViewBag.SelectedId = id;
             return View();
