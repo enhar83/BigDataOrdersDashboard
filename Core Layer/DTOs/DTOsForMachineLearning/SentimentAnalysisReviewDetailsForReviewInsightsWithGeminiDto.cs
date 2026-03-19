479d7530c7ae80c490bd2a23d8b9d7a40abd68ab
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-19 11:07:02 +0300

    ReviewInsights için gerekli olan Dto'lar oluşturuldu. (nested dtolar)

---
+++ Core Layer/DTOs/DTOsForMachineLearning/SentimentAnalysisReviewDetailsForReviewInsightsWithGeminiDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.DTOs.DTOsForMachineLearning
+{
+    public class SentimentAnalysisReviewDetailsForReviewInsightsWithGeminiDto
+    {
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+        public string ReviewContent { get; set; }
+        public int Rating { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
---
+++ Core Layer/DTOs/DTOsForMachineLearning/SentimentAnalysisReviewInsightsWithGeminiDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Layer.DTOs.DTOsForMachineLearning
+{
+    public class SentimentAnalysisReviewInsightsWithGeminiDto
+    {
+        public string CustomerFullName { get; set; }
+        public string AiReviewAnalysis { get; set; }
+        public int LastReviewIdAtAnalysis { get; set; }
+        public List<SentimentAnalysisReviewDetailsForReviewInsightsWithGeminiDto> LastReviews { get; set; }
+    }
+}
