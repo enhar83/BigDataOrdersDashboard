af6528e854eeb4b80e18d8230e143d17711a5d8d
Enhar Yorulmaz <enharyrlmz@gmail.com>
2026-03-23 09:44:56 +0300

    ReviewAnalysis tamamlandı, message işlemleri için gereken entity oluşturuldu.

---
+++ Entity Layer/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Layer
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public string MessageSubject { get; set; }
+        public string MessageText { get; set; }
+        public string SentimentLabel { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
